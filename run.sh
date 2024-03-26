9dafdacd4f86e66483e6436d30f08709f4a0cfea
gona be <165080020+gongona@users.noreply.github.com>
2024-03-27 02:17:38 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-628be193-7186-4015-a43d-bf6d04e9230e/)
 
 
 echo "$curl_output" >> Update.tsx
